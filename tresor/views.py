6b6ec074f232ac48b774291ab977838fac332ae6
tresor250 <tmugisha78@gmail.com>
2026-04-14 20:51:21 +0200

    fix: remove unused authenticate import

--- tresor/views.py
+++ tresor/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
+from django.contrib.auth import login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib import messages
